23d0e901668f427c2e30cf75c9b4607cbf7a439e
MCGiako <giacometto.r.h2@gmail.com>
2021-08-09 21:32:15 +0000

    code excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let random_who = Math.floor(Math.random() * who.length);
+
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let random_what = Math.floor(Math.random() * who.length);
+
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let random_when = Math.floor(Math.random() * when.length);
+
+  let result =
+    who[random_who] + " " + what[random_what] + " " + when[random_when];
+  document.getElementById("Excuse").innerHTML = result;
 };
